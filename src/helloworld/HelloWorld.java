be7e6ca52f910f7dd799597beeabbcfa5b1d8d0a
Maravilhosa <eloisawb19@gmail.com>
2017-03-17 20:36:25 -0300

    Alterada a mensagem de Hello para fins de exemplo.

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -19,6 +19,6 @@ package helloworld;
  */
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Ola, Mundo");
+		System.out.println("Ola, Mundo Teste");
 	}
 }
